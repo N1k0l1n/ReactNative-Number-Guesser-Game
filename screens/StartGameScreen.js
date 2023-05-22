fdf9eb814fd4ab55b7e68605cc5a3b81340d0eb1
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-05-22 15:07:33 +0200

    Game Basics

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,26 @@
+import { View, TextInput, StyleSheet } from "react-native";
+import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    padding: 16,
+    marginHorizontal: 24,
+    backgroundColor : '#72063c',
+    borderRadius : 8,
+    elevation: 4
+  },
+});
